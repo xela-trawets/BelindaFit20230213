62ad61a7c645d8015bf9659ac7aa4b9de584e24b
ideal.light <ideal.light@outlook.com>
2023-02-24 13:28:32 -0800

    Axes zero

--- Belinda.R
+++ Belinda.R
@@ -52,25 +52,40 @@ x_axis <- (1:100) * x_max * 0.01
 par(oma=c(2,2,2,2))
 par(mar=c(6,6,1,1))
 
-  col_list =  c("#00AFBB", "#E7B800", "#FC4E07")
+col_list =  c("#00AFBB", "#E7B800", "#FC4E07", "#4169E1", "#F08080")
+col1 = col_list[5]
+col2 = col_list[4]
 
-plot(df1$x, df1$y / sat1, cex = 2, pch=19, col = col_list[3], xlim = c(0, x_max), ylim = c(0, 1.5), 
+# plot(cx, cy, xaxt = "n")
+# axis(1, at = seq(0, 2, .5), labels = seq(0, 2, .5), lwd = 0, lwd.ticks = 1)
+# axis(1, at = setdiff(cx, seq(0, 2, .5)), labels = NA, lwd = NA, lwd.ticks = 1)
+# box()
+
+plot(df1$x, df1$y / sat1, 
+type = 'n',  
+frame = FALSE, cex = 2, pch=19,
+tick = FALSE, 
+col = col1, 
+xlim = c(0, x_max), ylim = c(0, 1.5), 
+
 #plot(df1$x, df1$y / sat1, pch = 19, col = 2, xlim = c(0, x_max), ylim = c(0, 1), 
 ###plot(df1$x, df1$y, pch = 19, col = 2, xlim = c(0, 5), ylim = c(0, 1.5), 
 
-
-xlab = '	Free Tm (µM) ', 
+xaxs = "i",
+yaxs = "i",
+xlab = '	Free Tropomyosin (µM) ', 
 ylab = 'Fractional Saturation (Tm/Actin)',
      cex.lab = 2.5,
      cex.axis = 2,
      cex.main = 1,
      cex.sub = 1)
 
-box(lwd=2.5)
+box(bty = "L", lwd = 4)
 
-lines(x_axis, predict(fm1, newdata = list(x = x_axis)) / sat1, col = 2, lwd = 3)
-lines(x_axis, predict(fm2, newdata = list(x = x_axis)) / sat2, col = 3, lwd = 3)
-points(df2$x, df2$y / sat2, cex = 1.9, pch = 19, col = 3)
+lines(x_axis, predict(fm1, newdata = list(x = x_axis)) / sat1, col = col1, lwd = 3)
+lines(x_axis, predict(fm2, newdata = list(x = x_axis)) / sat2, col = col2, lwd = 3)
+points(df2$x, df2$y / sat2, cex = 2.5, pch = 15, col = col2)
+points(df1$x, df1$y / sat2, cex = 2.3, pch = 19, col = col1)
 
 #lines(x_axis, predict(fm1, newdata = list(x = x_axis)) , col = 2, lwd = 3)
 #lines(x_axis, predict(fm2, newdata = list(x = x_axis)) , col = 3, lwd = 3)
@@ -79,7 +94,7 @@ points(df2$x, df2$y / sat2, cex = 1.9, pch = 19, col = 3)
 txt1 <- formatC(km1, digits = 4, format = "f")
 txt2 <- formatC(km2, digits = 4, format = "f")
 
-(km1 - km2)/sqrt(sekm1 * sekm1 + sekm2 * sekm2)
+(km1 - km2) / sqrt(sekm1 * sekm1 + sekm2 * sekm2)
 
 # legend("bottomright",
 # legend = c(txt1, txt2), col = c(2, 3), pch = 19, lwd = 3,
